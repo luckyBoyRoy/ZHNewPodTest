8a8a7ac3cfb4ceb9f330ca62d3659db4630badbb
roy <luckyBoyRoy@163.com>
2017-04-12 10:39:51 +0800

    测试版本1.0.0

---
+++ ZHNewPodTest/ZHTestObj.h
@@ -0,0 +1,13 @@
+//
+//  ZHTestObj.h
+//  ZHNewPodTest
+//
+//  Created by luckyRoy on 2017/4/11.
+//  Copyright © 2017年 object.Test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZHTestObj : NSObject
+
+@end
---
+++ ZHNewPodTest/ZHTestObj.m
@@ -0,0 +1,21 @@
+//
+//  ZHTestObj.m
+//  ZHNewPodTest
+//
+//  Created by luckyRoy on 2017/4/11.
+//  Copyright © 2017年 object.Test. All rights reserved.
+//
+
+#import "ZHTestObj.h"
+
+@implementation ZHTestObj
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        NSLog(@"this is a git test");
+    }
+    return self;
+}
+
+@end
